85217897c2e09fa131c034da0aa53261b58307fb
xmustafa5 <musmoh73@gmail.com>
2025-05-18 12:53:05 +0300

    Add demo data JSON file and update UI styles in confirmation dialogs

--- src/components/base/BaseDeleteDialog.vue
+++ src/components/base/BaseDeleteDialog.vue
@@ -33,22 +33,22 @@ async function handleConfirm() {
       role="dialog"
       aria-modal="true"
     >
-      <div class="fixed inset-0 bg-gray-500/75 transition-opacity" aria-hidden="true"></div>
+      <div class="fixed inset-0 bg-[#202020c2] transition-opacity" aria-hidden="true"></div>
 
       <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
         <div
           class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
         >
           <div
-            class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
+            class="relative transform overflow-hidden rounded-lg bg-[#131415] text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
           >
-            <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+            <div class="bg-[#131415] px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
               <div class="sm:flex sm:items-start">
                 <div
-                  class="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:size-10"
-                >
-                  <svg
-                    class="size-6 text-red-600"
+                  class="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-[#88071d] sm:mx-0 sm:size-10"
+                  >
+                    <svg
+                    class="size-6 text-[#ff516e]"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke-width="1.5"
@@ -64,18 +64,18 @@ async function handleConfirm() {
                   </svg>
                 </div>
                 <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                  <h3 class="text-base font-semibold text-gray-900" id="modal-title">
+                  <h3 class="text-base font-semibold text-[#e5e5e5]" id="modal-title">
                     {{ props.title || 'Delete' }}
                   </h3>
                   <div class="mt-2">
-                    <p class="text-sm text-gray-500">
+                    <p class="text-sm text-[#e5e5e5]">
                       {{ props.message || 'Are you sure you want to delete this item?' }}
                     </p>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+            <div class="bg-[#2c2d2e] px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
               <button
                 @click="handleConfirm"
                 type="button"
